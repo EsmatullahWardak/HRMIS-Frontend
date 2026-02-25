c1fa30455b83a78fab68c4b9ece4bd4773335395
Esmat Elham <esmatullah.wardak2020@gmail.com>
2026-02-25 16:16:49 +0430

    new chagnes applied

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
   ];
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 pb-10">
       {/* 1. Header Section */}
       <div className="flex flex-col md:flex-row md:items-center justify-between gap-4">
         <div>
