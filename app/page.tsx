e7c9660f728acc236baa69e3d02b60032a23fafd
Esmat Elham <esmatullah.wardak2020@gmail.com>
2026-02-26 13:57:26 +0430

    main route issue solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <p>this is the home route</p>
-  );
+export default function RootPage() {
+  redirect("/home");
 }
